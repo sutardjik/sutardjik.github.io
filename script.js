3d6231ba3ab589c90f31301d33f9e5b2e20506b1
sutardjik <sealion19@yahoo.com>
2023-12-21 16:43:36 +0800

    meta keys

--- script.js
+++ script.js
@@ -1,39 +1,39 @@
 document.addEventListener(
-    "contextmenu",
-    function (e) {
-      e.preventDefault();
-    },
-    false
+  "contextmenu",
+  function (e) {
+    e.preventDefault();
+  },
+  false
 );
 
 document.addEventListener(
-    "keydown",
-    function (e) {
-        if (e.ctrlKey && e.shiftKey && e.code === "KeyI" ||
-        e.ctrlKey && e.shiftKey && e.code === "KeyJ" || 
-        e.ctrlKey && e.code === "KeyU" ||
-        e.code === "Insert" ||
-        e.ctrlKey && e.code === "KeyC" ||
-        e.code === "KeyS" && (navigator.userAgent.includes("Macintosh") ? e.metaKey : e.ctrlKey)) {
-            disabledEvent(e);
-        }
-    },
-    false
+  "keydown",
+  function (e) {
+    if (e.code === "Insert" ||
+      e.code === "KeyI" && e.shiftKey && (navigator.userAgent.includes("Macintosh") ? e.metaKey : e.ctrlKey) ||
+      e.code === "KeyJ" && e.shiftKey && (navigator.userAgent.includes("Macintosh") ? e.metaKey : e.ctrlKey) || 
+      e.code === "KeyU" && (navigator.userAgent.includes("Macintosh") ? e.metaKey : e.ctrlKey) ||
+      e.code === "KeyC" && (navigator.userAgent.includes("Macintosh") ? e.metaKey : e.ctrlKey) ||
+      e.code === "KeyS" && (navigator.userAgent.includes("Macintosh") ? e.metaKey : e.ctrlKey)) {
+        disabledEvent(e);
+    }
+  },
+  false
 );
 
 function disabledEvent(e) {
-    e.stopPropagation ? e.stopPropagation() : (e.cancelBubble = true);
-    e.preventDefault();
-    return false;
+  e.stopPropagation ? e.stopPropagation() : (e.cancelBubble = true);
+  e.preventDefault();
+  return false;
 }
 
 const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-      console.log(entry);
-      if (entry.isIntersecting) {
-        entry.target.classList.add("show");
-      }
-    });
+  entries.forEach((entry) => {
+  console.log(entry);
+  if (entry.isIntersecting) {
+    entry.target.classList.add("show");
+  }
+  });
 });
 const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((el) => observer.observe(el));
